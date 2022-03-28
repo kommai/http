f494f6c57f68d8634fef38cff5d0505a6dcdf08d
Kommai <dev@kommai.app>
2022-03-28 16:22:17 +0900

    Modified

--- src/Response.php
+++ src/Response.php
@@ -7,9 +7,13 @@ namespace Kommai\Http;
 class Response
 {
     public const STATUS_OK = 200;
+    public const STATUS_FOUND = 302;
+    public const STATUS_NOT_FOUND = 404;
+    public const STATUS_INTERNAL_SERVER_ERROR = 500;
 
     public int $status;
     public array $headers;
+    /** @var Cookie[] $cookies */
     public array $cookies = [];
     public string $body;
 
