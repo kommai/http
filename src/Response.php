9835b863fbd1354c631a16d4521eacc7b5460d63
Kommai <dev@kommai.app>
2022-03-29 14:58:00 +0900

    Added 503

--- src/Response.php
+++ src/Response.php
@@ -11,6 +11,7 @@ class Response
     public const STATUS_FORBIDDEN = 403;
     public const STATUS_NOT_FOUND = 404;
     public const STATUS_INTERNAL_SERVER_ERROR = 500;
+    public const STATUS_SERVICE_UNAVAILABLE = 503;
 
     public int $status;
     public array $headers;
