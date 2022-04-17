fa6c0fb1126eaf1bcea00ac5b5f958000514d0fd
Kommai <dev@kommai.app>
2022-04-17 22:32:24 +0900

    Added

---
+++ tests/request.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Kommai\Http\Request;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$request = Request::createFromGlobals();
+echo sprintf('<pre>%s</pre>', var_export($request, true));
+echo '<hr>';
+echo sprintf('<pre>%s</pre>', var_export($_SERVER, true));
