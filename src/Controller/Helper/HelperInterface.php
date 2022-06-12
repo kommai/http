7d6f3a5861029205e991efa05e6408e1a66a3b2a
kommai <dev@kommai.app>
2022-06-13 08:28:08 +0900

    Branched

--- src/Controller/Helper/HelperInterface.php
+++ src/Controller/Helper/HelperInterface.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Kommai\Http\Controller\Helper;
 
-use Kommai\Http\Controller\ControllerInterface;
-
 interface HelperInterface
 {
-    //public function help(ControllerInterface $controller): void;
+    public function __get(string $name): mixed;
 }
