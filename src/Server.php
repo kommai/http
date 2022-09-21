0260e3c61ad88cf49e31a587a78ed3499a53a107
Kommai <dev@kommai.app>
2022-09-21 17:15:48 +0900

    Cookies first because headers MAY include redirect

--- src/Server.php
+++ src/Server.php
@@ -123,14 +123,14 @@ class Server
     private function sendResponse(Response $response): void
     {
         http_response_code($response->status);
-        foreach ($response->headers as $name => $value) {
-            header(sprintf('%s: %s', $name, $value));
-        }
         foreach ($response->cookies as $cookie) {
             if (!$cookie instanceof Cookie) {
                 throw new DomainException('Invalid cookie');
             }
             setcookie($cookie->name, $cookie->value, $cookie->options);
+        }
+        foreach ($response->headers as $name => $value) {
+            header(sprintf('%s: %s', $name, $value));
         }
         echo $response->body;
     }
